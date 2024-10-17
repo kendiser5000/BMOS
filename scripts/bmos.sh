4f5aaba4bdb02c221c6341790e9ba0e4badea976
kendiser5000 <ashraf1sikender@utexas.edu>
2024-10-16 22:53:44 -0700

    Update boot script

--- scripts/bmos.sh
+++ scripts/bmos.sh
@@ -3,13 +3,10 @@ then
    /usr/bin/aplay -D default -t raw -r 48000 -c 2 -f S16_LE  /dev/zero &
 fi
 
-touch /home/pi/bmos/parade/parade.txt
+# touch /home/pi/bmos/parade/parade.txt
 
-/home/pi/bmos/scripts/armsinit.sh
-/home/pi/bmos/scripts/armsupslow.sh &
-# omxplayer --aspect-mode fill --fps 25 --layer 10010 --no-keys --no-osd /home/pi/bmos/videos/intro.mp4 > /dev/null &
-cvlc --aspect-ratio 16:9 /home/pi/bmos/videos/intro.mp4 vlc://quit > /dev/null &
-
+omxplayer --aspect-mode fill --fps 25 --layer 10010 --no-keys --no-osd /home/pi/bmos/videos/intro.mp4 > /dev/null &
+# cvlc --aspect-ratio 16:9 /home/pi/bmos/videos/intro.mp4 vlc://quit > /dev/null &
 
 cd /home/pi/bmos/
 /home/pi/bmos/bmos
