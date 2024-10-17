3c3f08fdb8f95533adc04f8a9ebc40f98eb41acd
kendiser5000 <ashraf1sikender@utexas.edu>
2024-10-16 23:57:22 -0700

    Update emulaunch.sh

--- scripts/emulaunch.sh
+++ scripts/emulaunch.sh
@@ -1,4 +1,4 @@
 /usr/bin/emulationstation
-omxplayer --aspect-mode fill --fps 25 --layer 10010 --no-keys --no-osd /home/pi/bmos/videos/videogames.mp4 > /dev/null &
+omxplayer --aspect-mode fill --fps 25 --layer 10010 --no-keys --no-osd /home/pi/bmos/videos/videogames.mp4 > /dev/null
 /home/pi/bmos/bmos &
 omxplayer --aspect-mode fill --fps 25 --layer 10010 --no-keys --no-osd /home/pi/bmos/videos/gameover.mp4 > /dev/null &
